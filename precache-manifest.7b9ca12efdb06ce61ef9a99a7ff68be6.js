f4e1c5cab4a8fa74cbe943a30e6b523a63dc8484
lf639 <lf639@bath.ac.uk>
2020-12-17 14:04:18 +0000

    Updates

---
+++ precache-manifest.7b9ca12efdb06ce61ef9a99a7ff68be6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48807d230d7aa5c7dcc8b9a61f94209d",
+    "url": "/tomato-timer/index.html"
+  },
+  {
+    "revision": "81a1626a0bf311dcefcd",
+    "url": "/tomato-timer/static/css/main.d084bc57.chunk.css"
+  },
+  {
+    "revision": "25c341eb02b89122b425",
+    "url": "/tomato-timer/static/js/2.cffc6bf1.chunk.js"
+  },
+  {
+    "revision": "81a1626a0bf311dcefcd",
+    "url": "/tomato-timer/static/js/main.5209661a.chunk.js"
+  },
+  {
+    "revision": "55a7baba040effcb8af4",
+    "url": "/tomato-timer/static/js/runtime-main.85f4ce74.js"
+  },
+  {
+    "revision": "cc92724288ed763e4acbe79a5ca6d7da",
+    "url": "/tomato-timer/static/media/AlarmClock.cc927242.wav"
+  },
+  {
+    "revision": "775ea83c942fb720f16d32a33f47ed0a",
+    "url": "/tomato-timer/static/media/DoorBell.775ea83c.wav"
+  },
+  {
+    "revision": "4f595db9cae6c94982a4958bdc472361",
+    "url": "/tomato-timer/static/media/EightiesAlarm.4f595db9.wav"
+  },
+  {
+    "revision": "4dd0cef9b66e818c66b3a44afc33657a",
+    "url": "/tomato-timer/static/media/ElevatorDing.4dd0cef9.wav"
+  },
+  {
+    "revision": "3952ef9f46afc8c384f5b4f82e16d3ab",
+    "url": "/tomato-timer/static/media/WristwatchAlarm.3952ef9f.wav"
+  }
+]);
